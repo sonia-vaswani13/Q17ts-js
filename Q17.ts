727ddfa67bd183f9a010d9d00393681df296da1c
sonia-vaswani13 <163179929+sonia-vaswani13@users.noreply.github.com>
2024-04-29 13:22:28 -0700

    Q17 ts/js node js of 45 assignment
    Q17 ts/js node js of 45 assignment

---
+++ Q17.ts
@@ -0,0 +1,20 @@
+//Shrinking Guest List: You just found out that your new dinner table won’t arrive in time for the dinner, and you have space for only two guests.
+//• Start with your program from Exercise 16.
+//Add a new line that prints a message saying that you can invite only two people for dinner.
+
+let frndsArr =["Drsuman", "Drsonia","Dr kiran","Dr jagdeep"]
+console.log("I can invite only two people on Dinner");
+ 
+//• Remove guests from your list one at a time until only two names remain in your list. Each time you pop a name from your list, print a message to that person letting them know you’re sorry you can’t invite them to dinner.
+while(frndsArr.length > 2){
+    let removeGuest =frndsArr.pop();
+    console.log(`Dear ${removeGuest},you are not invited`);
+}
+//• Print a message to each of the two people still on your list, letting them know they’re still invited.
+//• Remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program.
+frndsArr.map((item) => console.log(`Dear ${item},you are still invited to the dinner`));
+frndsArr.pop();
+console.log(frndsArr);
+
+
+
